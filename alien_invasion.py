84e19b4b6c50ea8a710a78eb93d50c1413a37149
per14fect <kozlova14@gmail.com>
2024-07-18 15:57:31 +0200

    add settings

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+from settings import Settings
+
+class AlienInvasion():
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, 
+                                               self.settings.screen_height))
+        pygame.display.set_caption('Alien Invasion')
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
