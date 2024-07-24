6e89a52dcd0e69053004fc02bf9506d8e349ab14
per14fect <kozlova14@gmail.com>
2024-07-24 11:01:20 +0200

    update settings

--- settings.py
+++ settings.py
@@ -4,15 +4,26 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
-        self.ship_speed = 4.5
         self.ship_limit = 3
 
-        self.bullet_speed = 6.0
-        self.bullet_width = 100.0
+        self.bullet_width = 3000.0
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
 
-        self.alien_speed = 10
         self.fleet_drop_speed = 10
+
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.0
+        self.alien_speed = 1
+
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
